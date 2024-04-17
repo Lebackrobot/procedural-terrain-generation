1ceb9dd0ce0c7a1a8ed6dd3b2dc8c8e2ca98c620
Lebackrobot <lebackman18@gmail.com>
2024-04-16 23:04:51 -0300

    feat: noise adjustment

--- src/modules/terrain.js
+++ src/modules/terrain.js
@@ -4,29 +4,29 @@ class Terrain {
             case 'water':
                 this.minHeight = 0.2
                 this.maxHeight = 0.4
-                this.maxColor = color(30, 176, 251)
-                this.minColor = color(40, 255, 255)
+                this.minColor = color(30, 176, 251)
+                this.maxColor = color(40, 255, 255)
                 break
 
             case 'sand':
                 this.minHeight = 0.4
                 this.maxHeight = 0.5             
-                this.maxColor = color(215, 192, 158)
-                this.minColor = color(255, 246, 193)
+                this.minColor = color(215, 192, 158)
+                this.maxColor = color(255, 246, 193)
                 break
 
             case 'grass':
                 this.minHeight = 0.5
                 this.maxHeight = 0.7
-                this.maxColor = color(2, 166, 155)
-                this.minColor = color(118, 239, 124)
+                this.minColor = color(2, 166, 155)
+                this.maxColor = color(118, 239, 124)
                 break
 
             case 'trees':
                 this.minHeight = 0.7
-                this.maxHeight = .85
-                this.maxColor = color(22, 181, 141)
-                this.minColor = color(10, 145, 113)
+                this.maxHeight = .75
+                this.minColor = color(22, 181, 141)
+                this.maxColor = color(10, 145, 113)
                 break
         }
     }
@@ -41,7 +41,7 @@ class Terrain {
     getColorByNoiseValue(value) {
         const normalized = this.#normalize(this.minHeight, this.maxHeight, value)
         
-        return lerpColor(this.minColor, this.maxColor, normalized/5)
+        return lerpColor(this.minColor, this.maxColor, normalized)
     
     }
 }
